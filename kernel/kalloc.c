7a42441c7149b93de9a46e5b839d6f094a77eb49
caoyaoyuan <2749322671@qq.com>
2022-08-10 14:21:11 +0800

    cow

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,16 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct ref {
+  struct spinlock lock;
+  int cnt[PHYSTOP / PGSIZE]; // 引用计数
+} ref;
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&ref.lock, "ref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +41,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    ref.cnt[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,15 +59,21 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  acquire(&ref.lock);
+  if (--ref.cnt[(uint64)pa / PGSIZE] == 0) {
+    release(&ref.lock);
+    r = (struct run*)pa;
+    
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
-
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  } else {
+    release(&ref.lock);
+  }
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -72,11 +86,87 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r) {
     kmem.freelist = r->next;
+    acquire(&ref.lock);
+    ref.cnt[(uint64)r / PGSIZE] = 1;
+    release(&ref.lock);
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+// 判断一个页面是否是COW页面
+int 
+is_cow_page(pagetable_t pagetable, uint64 va) 
+{
+
+  if (va >= MAXVA)
+    return -1;
+  
+  pte_t *pte = walk(pagetable, va, 0);
+  if (pte == 0)
+    return -1;
+  if ((*pte &PTE_V) == 0)
+    return -1;
+ 
+  return (*pte & PTE_COW ? 0 : -1);
+}
+
+// copy-on-write 分配器
+void*
+cow_alloc(pagetable_t pagetable, uint64 va)
+{
+  if (va % PGSIZE != 0)
+    return 0;
+  
+  uint64 pa = walkaddr(pagetable, va);
+  if (pa == 0)
+    return 0;
+
+  pte_t* pte = walk(pagetable, va, 0);
+
+  if (krefcnt((char*)pa) == 1) {
+    *pte |= PTE_W;
+    *pte &= ~PTE_COW;
+    return (void*)pa;
+  } else {
+    char* mem = kalloc();
+    if (mem == 0)
+      return 0;
+    
+    memmove(mem, (char*)pa, PGSIZE);
+
+    *pte &= ~PTE_V;
+
+    if (mappages(pagetable, va, PGSIZE, (uint64)mem, (PTE_FLAGS(*pte) | PTE_W) & ~PTE_COW) != 0) {
+      kfree(mem);
+      *pte |= PTE_V;
+      return 0;
+    }
+    kfree((char*)PGROUNDDOWN(pa));
+    return mem;
+  }
+}
+
+// 获取内存引用计数
+int
+krefcnt(void* pa)
+{
+  return ref.cnt[(uint64)pa / PGSIZE];
+}
+
+// 增加内存引用计数
+int 
+kaddrefcnt(void* pa)
+{
+  if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    return -1;
+  acquire(&ref.lock);
+  ++ref.cnt[(uint64)pa / PGSIZE];
+  release(&ref.lock);
+  return 0;
 }
